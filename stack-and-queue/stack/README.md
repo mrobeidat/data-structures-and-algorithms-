fd1a944fbf7c64f1754f5a5e0748d759059b30b1
Yousef Obeidat <y.linux96@gmail.com>
2021-11-12 19:11:24 +0200

    finished

---
+++ stack-and-queue/stack/README.md
@@ -0,0 +1,29 @@
+# Stack_and_Queue
+
+Implementation: Stack_and_Queue
+## Challenge
+- [x]    Can successfully push onto a stack
+- [x]   Can successfully push multiple values onto a stack
+- [x]    Can successfully pop off the stack
+- [x]    Can successfully empty a stack after multiple pops
+- [x]    Can successfully peek the next item on the stack
+- [x]    Can successfully instantiate an empty stack
+- [x]    Calling pop or peek on empty stack raises exception
+- [x]    Can successfully enqueue into a queue
+- [x]    Can successfully enqueue multiple values into a queue
+- [x]    Can successfully dequeue out of a queue the expected value
+- [x]    Can successfully peek into a queue, seeing the expected value
+- [x]    Can successfully empty a queue after multiple dequeues
+- [x]    Can successfully instantiate an empty queue
+- [x]   Calling dequeue or peek on empty queue raises exception
+
+## Approach & Efficiency
+time=o(n)
+space=o(1)
+## API
++ push method :adds a new node with that value to the top of the stack with an O(1) Time performance.
++ pop method:Removes the node from the top of the stack.
++ peek :Returns Value of the node located at the top of the stack.
++ is empty:Returns Boolean indicating whether or not the stack is empty.
++ enqueue:adds a new node with that value to the back of the queue with an O(1) Time performance.
++ dequeue :Removes the node from the front of the queue. 
--- stack-and-queue/stack/stack_and_queue/stack_and_queue.py
+++ stack-and-queue/stack/stack_and_queue/stack_and_queue.py
@@ -1,36 +1,72 @@
 
-class Node:
+# class Node:
 
-    def __init__(self, value=None):
-        self.next=None
-        self.value=value
+#     def __init__(self, value=None):
+#         self.next=None
+#         self.value=value
 
 
-class Stack:
-    """
-    a class that implements the Stack Data structure
-    """
-    def __init__(self):
-        self.top=None
+# class Stack:
+#     """
+#     a class that implements the Stack Data structure
+#     """
+#     def __init__(self):
+#         self.top=None
     
-    def push(self, value):
-        node = Node(value)
-        if self.top:
-            node.next=self.top
+#     def push(self, value):
+#         node = Node(value)
+#         if self.top:
+#             node.next=self.top
 
-        self.top=node
+#         self.top=node
             
 
-    def pop(self):
+#     def pop(self):
 
-        if self.top == None:
-          raise EmptyStack("This stack is empty")
-        temp =self.top
-        self.top=self.top.next
-        temp.next=None
-        return temp.value        
+#         if self.top == None:
+#           raise EmptyStack("This stack is empty")
+#         temp =self.top
+#         self.top=self.top.next
+#         temp.next=None
+#         return temp.value        
        
 
+
+#     def peek(self):
+#        if self.is_empty():
+#         raise Exception("This stack is empty")
+#        return self.top.value
+
+#     def is_empty(self):
+#         return not self.top
+
+#     def is_empty(self):
+#         pass
+
+class Node:
+    def __init__(self, value, next = None):
+        self.value=value
+        self.next=None
+
+
+"""Stack class and methdos"""
+class Stack:
+
+    def __init__(self):
+        self.top=None
+
+    def push(self, value):
+        node=Node(value)
+        node.next=self.top
+        self.top=node
+
+    def pop(self):
+        if self.top == None:
+            raise Exception("This stack is empty")
+        temp=self.top
+        self.top=self.top.next
+        temp.next=None
+        return temp.value
 
     def peek(self):
        if self.is_empty():
@@ -40,34 +76,67 @@ class Stack:
     def is_empty(self):
         return not self.top
 
-    def is_empty(self):
-        pass
 
+# class Queue:
+#     """
+#     a class that implements the Queue Data structure
+#     """
+#     def __init__(self):
+#         self.front = None
+#         self.rear = None
+    
+    
+#     def enqueue(self, value):
+#         node=Node(value)
+
+#         if not self.rear:
+#             self.front = node
+#             self.rear = node
+#         else:
+#             self.rear.next = node
+#             self.rear = node
+    
+#     def dequeue(self):
+#         pass
+    
+#     def peek(self):
+#         pass
+
+#     def is_empty(self):
+#         return self.front == None
 
 class Queue:
-    """
-    a class that implements the Queue Data structure
-    """
     def __init__(self):
-        self.front = None
-        self.rear = None
-    
-    
+        self.front=None
+        self.rear=None
+
+
     def enqueue(self, value):
         node=Node(value)
-
-        if not self.rear:
-            self.front = node
-            self.rear = node
+        if not self.front:
+            self.front=node
+            self.rear=node
         else:
-            self.rear.next = node
-            self.rear = node
-    
+            self.rear.next=node
+            self.rear=node
+
+
     def dequeue(self):
-        pass
-    
+        if self.is_empty():
+         raise Exception("This stack is empty")
+        if self.front is None:
+            return None
+        front = self.front.value
+        self.front = self.front.next
+        return front
+
+
     def peek(self):
-        pass
+        if self.is_empty():
+          raise Exception("This stack is empty")
+        return self.front.value
 
     def is_empty(self):
-        return self.front == None
+        return not self.front
+
+
