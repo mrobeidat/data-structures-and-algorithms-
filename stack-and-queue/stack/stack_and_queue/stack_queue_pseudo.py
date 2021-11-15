d8110fbb00e42e08aa36d6a1fe3a09c2847135ba
Yousef Obeidat <y.linux96@gmail.com>
2021-11-15 18:59:01 +0200

    fix

--- stack-and-queue/stack/stack_and_queue/stack_queue_pseudo.py
+++ stack-and-queue/stack/stack_and_queue/stack_queue_pseudo.py
@@ -1,71 +1,96 @@
 class Node:
-            def __init__(self, value):
-                self.value = value
-                self.next = None
+    def __init__(self, value):
+        self.value = value
+        self.next = None
 
-class pseudo_queue(object):
-              
-                def __init__(self, size):
-                    self.stack1=[]
-
-                    self.stack2=[]
-
-                    self.queue = []
-
-                    self.size = size
-
-                def enqueue(self, value):
-                    if(self.isFull() != True):
-                        self.queue.insert(0, value)
-                    else:
-                        print('Queue is Full!')
+####################################################################
 
 
+class Stack:
+    def __init__(self):
+        self.top = None
 
-                def dequeue(self):
-                    if(self.isEmpty() != True):
-                        return self.queue.pop()
-                    else:
-                        print('Queue is Empty!')
+    def push(self, value):
+        node = Node(value)
+        if self.top == None:
+            self.top = node
+        else:
+            node.next = self.top
+            self.top = node
+
+    def pop(self):
+        if not self.isEmpty():
+            temp = self.top
+            self.top = self.top.next
+            temp.next = None
+            return temp.value
+        else:
+            return "This is Empty stack"
+
+    def isEmpty(self):
+        if self.top == None:
+            return True
+        else:
+            return False
+
+    def peek(self):
+        try:
+            return self.top.value
+        except:
+            return "This is Empty stack"
+
+####################################################################
 
 
+class PseudoQueue:
+    def __init__(self):
+        self.first_stack = Stack()
+        self.secand_stack = Stack()
+        self.rear = None
+        self.front = None
 
-                def peek(self):
-                    if(self.isEmpty() != True):
-                        return self.queue[-1]
-                    else:
-                        print('Queue is Empty!')
+    def enqueue(self, value):
+        self.first_stack.push(value)
+        self.rear = self.first_stack.top.value
+
+    def dequeue(self):
+        if self.first_stack.top:
+            stack1 = self.first_stack
+            while not stack1.isEmpty():
+                self.secand_stack.push(stack1.pop())
+
+            poped = self.secand_stack.pop()
+            self.front = self.secand_stack.top
+            self.first_stack = Stack()
+            stack2 = self.secand_stack
+            while not stack2.isEmpty():
+                self.first_stack.push(stack2.pop())
+            return poped
+
+    def __str__(self):
+        content = ''
+        current = self.first_stack.top
+        while current:
+            content += f"{{{str(current.value)}}} -> "
+            current = current.next
+        content += " Null"
+        return content
+
+####################################################################
+####################################################################
 
 
-
-                def isEmpty(self):
-                    return self.queue == []
-
-
-
-                def isFull(self):
-                    return len(self.queue) == self.size
-
-
-
-                def __str__(self):
-                    myString = ' '.join(str(i) for i in self.queue)
-                    return myString
-
-
-
-if __name__ == '__main__':
-  myQueue = pseudo_queue(10)
-  myQueue.enqueue(20)
-  myQueue.enqueue(15)
-  myQueue.enqueue(10)
-
-  print(myQueue)
-
-  myQueue.enqueue(5)
-
-  print(myQueue)
-
-  myQueue.dequeue()
-
-  print(myQueue)
+if __name__ == "__main__":
+    queue = PseudoQueue()
+    queue.enqueue(5)
+    print(queue.__str__())
+    queue.enqueue(7)
+    print(queue.__str__())
+    queue.enqueue(9)
+    print(queue.__str__())
+    queue.dequeue()
+    print(queue.__str__())
+    queue.dequeue()
+    print(queue.__str__())
+    queue.dequeue()
+    print(queue.__str__())
--- stack-and-queue/stack/tests/test_pseudo.py
+++ stack-and-queue/stack/tests/test_pseudo.py
@@ -1,17 +1,38 @@
-# import pytest
-# from stack_and_queue.stack_queue_pseudo import Pseudo_queue
+
+from stack_and_queue.stack_queue_pseudo import PseudoQueue
+
+import pytest
+
+def test_enqueue_singl():
+    Pseudoqueue = PseudoQueue()
+    Pseudoqueue.enqueue(21)
+    actual = Pseudoqueue.rear
+    expected = 21
+    assert expected == actual
+
+def test_enqueue_multiple():
+    Pseudoqueue = PseudoQueue()
+    Pseudoqueue.enqueue(21)
+    Pseudoqueue.enqueue(22)
+    Pseudoqueue.enqueue(23)
+    actual = Pseudoqueue.rear
+    expected = 23
+    assert expected == actual
 
 
+def test_dequeue_ps():
+    Pseudoqueue = PseudoQueue()
+    Pseudoqueue.enqueue(1)
+    Pseudoqueue.enqueue(2)
+    Pseudoqueue.enqueue(3)
+    Pseudoqueue.dequeue()
+    Pseudoqueue.dequeue()
+    actual = Pseudoqueue.dequeue()
+    expected = 3
+    assert expected == actual
 
-# # @pytest.fixture
-# def test_cases_pseudo():
-#     queue = Pseudo_queue()
-#     queue.enqueue(1)
-#     queue.enqueue(2)
-#     queue.enqueue(3)
-#     queue.enqueue(4)
-#     queue.enqueue(5)
-#     assert queue.dequeue() == 1
-#     assert queue.dequeue() == 2
-#     assert queue.dequeue() == 3
-
+def test_dequeue_empty():
+    Pseudoqueue = PseudoQueue()
+    actual = Pseudoqueue.dequeue()
+    expected = None
+    assert expected == actual 
