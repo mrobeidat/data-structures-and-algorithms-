17936eea5c3eb91ed9481d84b5cdb773cb8de079
Yousef Obeidat <y.linux96@gmail.com>
2021-12-10 13:36:51 +0200

    hash_table

---
+++ hashtable/hashtable/hashtable.py
@@ -0,0 +1,72 @@
+class Node:
+    def __init__(self, value=None, next_=None):
+    
+        self.value = value
+        self.next = next_
+
+class LinkedList:
+    def __init__(self):
+     
+        self.head = None
+
+    def insert(self, value):
+      
+        self.head = Node(value, self.head)
+
+class HashTable:
+    def __init__(self, size=1024):
+      
+        self.__size = size
+        self.__buckets = [None] * size
+
+    def add(self, key, value):
+      
+        index = self.__hash(key)
+
+        if not self.__buckets[index]:
+          self.__buckets[index] = LinkedList()
+
+        my_value = [key,value]
+        self.__buckets[index].insert(my_value)
+
+    def get(self, key):
+      index = self.__hash(key)
+
+      if self.__buckets[index]:
+
+        linked_list = self.__buckets[index]
+        current = linked_list.head
+
+        while current:
+          if current.value[0] == key:
+            return current.value[1]
+            
+          current = current.next
+
+      return None
+
+
+    def contains(self,key):
+
+        index = self.__hash(key)
+
+        if not self.__buckets[index]:
+            return False
+
+        linked_list = self.__buckets[index]
+        current = linked_list.head
+        while current:
+
+          if current.value[0] == key:
+
+            return True
+          current = current.next
+    def __hash(self, key):
+ 
+        return sum([ord(char) for char in key]) * 7 % self.__size
+
+
+if __name__ == "__main__":
+    table = HashTable()
+    table.add('Yousef' , 25)
+    print(table.get('Yousef'))
